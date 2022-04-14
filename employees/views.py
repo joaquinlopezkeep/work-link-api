12452556d15cf3b650f3bb22e0d35f0a52c69e69
joaquin <joaquinlopezkeep@gmail.com>
2022-04-14 13:12:30 +0100

    changes to Employee view

--- employees/views.py
+++ employees/views.py
@@ -7,10 +7,11 @@ from rest_framework import viewsets
 from .models import Employee
 from .serializers import EmployeeSerializer, GroupSerializer
 from decouple import config
+import os
 
 
-CLIENT_ID = config('CLIENT_ID')
-CLIENT_SECRET = config('CLIENT_SECRET')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
 
 IP_token = 'http://127.0.0.1:8000/o/token/'
 
