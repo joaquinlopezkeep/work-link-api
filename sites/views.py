106ea8909f2b425587154f3ed3df054d7bc6181a
joaquin <joaquinlopezkeep@gmail.com>
2022-04-16 19:43:01 +0100

    removed filter in schedule view

--- sites/views.py
+++ sites/views.py
@@ -42,8 +42,6 @@ class JobViewSet(viewsets.ReadOnlyModelViewSet):
 class ScheduleViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Schedule.objects.all()
     serializer_class = ScheduleSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ['cleaner__email', 'site__name']
 
     def get_queryset(self):
         current_user = self.request.user
